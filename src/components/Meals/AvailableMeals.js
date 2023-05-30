fc0896e2ac92f96de8080d585f1697f76b01f7ef
Alex Kalen <alexkalen96@gmail.com>
2023-05-29 22:15:44 -0400

    Added firebase functionality

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -3,23 +3,54 @@ import classes from "./AvailableMeals.module.css";
 import MealItem from "./MealItem";
 
 import Card from "../UI/Card/Card";
+import { useCallback, useEffect, useState } from "react";
 
-function AvailableMeals(props) {
-  const addMealToCart = (addedMeal) => {
-    props.onNewMeal(addedMeal);
-  };
+function AvailableMeals() {
+  const [meals, setMeals] = useState([]);
+  const [error, setError] = useState(null);
+
+  const fetchMealsHandler = useCallback(async () => {
+    setError(null);
+    try {
+      const response = await fetch(
+        "https://react-http-3e360-default-rtdb.firebaseio.com/meals.json"
+      );
+
+      if (!response.ok) {
+        throw new Error("Something went wrong..");
+      }
+
+      const data = await response.json();
+      const loadedMeals = [];
+
+      for (const key in data) {
+        loadedMeals.push({
+          id: key,
+          name: data[key].name,
+          description: data[key].description,
+          price: data[key].price,
+        });
+      }
+      setMeals(loadedMeals);
+    } catch (error) {
+      setError(error.message);
+    }
+  }, []);
+
+  useEffect(() => {
+    fetchMealsHandler();
+  }, [fetchMealsHandler]);
 
   return (
     <Card className={classes.meals}>
       <ul>
-        {props.meals.map((meal) => (
+        {meals.map((meal) => (
           <MealItem
             key={meal.id}
             id={meal.id}
             mealName={meal.name}
             mealDescription={meal.description}
             mealPrice={meal.price}
-            onAddMeal={addMealToCart}
           />
         ))}
       </ul>
